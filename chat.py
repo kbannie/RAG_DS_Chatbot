c0b99a3718a2920495f53b3408727d1dc17d8ab7
가비 <kabi@gabiui-MacBookAir.local>
2025-02-19 20:43:10 +0900

    [Feat] Add Chat History

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from dotenv import load_dotenv
-from llm import get_ai_message
+from llm import get_ai_response
 
 st.set_page_config(page_title="덕성여대 규정 챗봇", page_icon="🤖")
 
@@ -19,13 +19,14 @@ for message in st.session_state.message_list:
 
 
 
+
 if user_question := st.chat_input(placeholder="덕성여대 규정에 관련된 궁금한 내용들을 말씀해주세요!"):
     with st.chat_message("user"):
         st.write(user_question)
     st.session_state.message_list.append({"role": "user", "content": user_question})
 
     with st.spinner("답변을 생성하는 중입니다"):
-        ai_message = get_ai_message(user_question)
+        ai_response = get_ai_response(user_question)
         with st.chat_message("ai"):
-            st.write(ai_message)
+            ai_message = st.write_stream(ai_response)
             st.session_state.message_list.append({"role": "ai", "content": ai_message})
